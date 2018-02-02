f37b802332722e6a705f20474377985ea05af179
kiquetal <kiquetal@gmail.com>
2018-02-01 23:16:54 -0300

    fun

--- src/Testing.kt
+++ src/Testing.kt
@@ -1,4 +1,14 @@
-data class Holder(val name:String="",val age:Int=0)
+data class Holder(val name:String="",val age:Int=0){
+
+    fun combine(acc: Holder): Holder =
+            if (age == acc.age)
+                Holder(acc.name + "-" +name ,age+acc.age)
+            else
+                throw IllegalStateException("Age don't match.")
+
+
+
+}
 
 
 object toTest
@@ -9,6 +19,6 @@ object toTest
         val l=listOf(Holder("juan",12),Holder("pedro",12),Holder("julian",14))
         println(l.groupBy { it.age }
                 .values
-                .map { it.reduce { acc, holder ->  Holder(holder.name + acc.name,holder.age+acc.age)  } })
+                .map { it.reduce (Holder::combine ) })
      }
 }
