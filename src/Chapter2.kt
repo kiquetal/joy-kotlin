926b50348bcec74c26c82a1005578d9f4a409780
kiquetal <kiquetal@gmail.com>
2018-03-31 16:59:55 -0400

    fun

--- src/Chapter2.kt
+++ src/Chapter2.kt
@@ -1,4 +1,5 @@
-
+import chapter2.FunList.Cons
+import chapter2.FunList.Nil
 object chapter2
 {
 
@@ -6,8 +7,44 @@ object chapter2
      fun main(args:Array<String>):Unit {
 
 
-}
 
+
+         val funList= inListOf(1,2,3,4,5)
+        println("La lista es $funList")
+        funList.forEach { println(it*2) }
+     }
+
+    fun inListOf(vararg numbers:Int):FunList<Int>{
+        return if (numbers.isEmpty()){
+            Nil
+        }
+        else {
+            Cons(numbers.first(), inListOf(* numbers.drop(1).toIntArray()))
+        }
+    }
+
+
+    sealed class FunList<out T> {
+
+        object Nil: FunList<Nothing>()
+        data class Cons<out T>(val head:T,val tail:FunList<T>):FunList<T>()
+
+        fun forEach(f:(T)->Unit) {
+            tailrec fun go(list:FunList<T>,f:(T)->Unit)
+            {
+                when (list)
+                {
+                    is Nil -> Unit
+                    is Cons-> {
+                        f(list.head)
+                        go(list.tail, f)
+                    }
+                }
+            }
+            go(this,f)
+        }
+
+    }
 
     open class Parent { fun alto():String="hola"}
     class MyClass:Parent()
